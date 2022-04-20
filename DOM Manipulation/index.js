e9b8cf9e9697b5ec7b33fb9cef3a6b82be61b054
Micah Yasharahla <mrmariomurray@gmail.com>
2022-04-19 21:38:20 -0400

    made the dom manipulation exercise bullet proof

--- DOM Manipulation/index.js
+++ DOM Manipulation/index.js
@@ -1,5 +1,5 @@
 
-// changing heading and the third li
+// Changing Heading and the Third Li
 function changeName() {
     var changeLi = document.querySelector('ul').lastElementChild
     var greeting = document.firstElementChild.lastElementChild.firstElementChild;
@@ -7,18 +7,25 @@ function changeName() {
         changeLi.innerHTML = "Third"
             
    
-    if (greeting.innerHTML === "Hello" && changeLi.innerHTML === "Third") {
+    // if (greeting.innerHTML === "Hello" && changeLi.innerHTML === "Third") {
          userName =  prompt("What is your name?")
-   
+   console.log(userName);
+   if (userName === "" || userName === null) {
+       greeting.innerHTML = "Hello"
+        changeLi.innerHTML = "Third"
+   } else {
         greeting.innerHTML = userName;
          changeLi.innerHTML = userName;
-    } else if (greeting.innerHTML === "" && changeLi.innerHTML === "") {
-        greeting.innerHTML = "Hello"
-        changeLi.innerHTML = "Third"
-    }
+    }  
 
     //Checking a Box
     document.querySelector("input").click();
+   
+    //Changing Link Color 
+    document.querySelector('ul li a').style.color = "red";
+
+    // Changing a button BG color
+    document.querySelector(".btn").style.backgroundColor = "yellow";
 }
 
 changeName();
